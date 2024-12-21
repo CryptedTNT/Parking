daab51557bb5d9e77663c020479e15a6ff97849d
CryptedTNT <ardardard27@gmail.com>
2024-12-21 22:25:37 +0800

    Add project files.

---
+++ Parking/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Parking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.parkBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.parkingGridView = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.waitingBack = new System.Windows.Forms.Button();
+            this.waitingGridView = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.historyGridView = new System.Windows.Forms.DataGridView();
+            this.Action = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.licensePlate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.parkingGridView)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waitingGridView)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.historyGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.parkBack);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtLicense);
+            this.panel1.Controls.Add(this.btnRemove);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.parkingGridView);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // parkBack
+            // 
+            this.parkBack.Location = new System.Drawing.Point(374, 355);
+            this.parkBack.Name = "parkBack";
+            this.parkBack.Size = new System.Drawing.Size(75, 23);
+            this.parkBack.TabIndex = 5;
+            this.parkBack.Text = "Back";
+            this.parkBack.UseVisualStyleBackColor = true;
+            this.parkBack.Click += new System.EventHandler(this.parkBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter License Plate #:";
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.Location = new System.Drawing.Point(348, 273);
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.Size = new System.Drawing.Size(100, 20);
+            this.txtLicense.TabIndex = 3;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(410, 301);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click_1);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(304, 301);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // parkingGridView
+            // 
+            this.parkingGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.parkingGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column1,
+            this.Status});
+            this.parkingGridView.Location = new System.Drawing.Point(229, 101);
+            this.parkingGridView.Name = "parkingGridView";
+            this.parkingGridView.Size = new System.Drawing.Size(344, 150);
+            this.parkingGridView.TabIndex = 0;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Slot";
+            this.Column2.Name = "Column2";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "License Plate";
+            this.Column1.Name = "Column1";
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.waitingBack);
+            this.panel2.Controls.Add(this.waitingGridView);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // waitingBack
+            // 
+            this.waitingBack.Location = new System.Drawing.Point(348, 316);
+            this.waitingBack.Name = "waitingBack";
+            this.waitingBack.Size = new System.Drawing.Size(75, 23);
+            this.waitingBack.TabIndex = 1;
+            this.waitingBack.Text = "Back";
+            this.waitingBack.UseVisualStyleBackColor = true;
+            this.waitingBack.Click += new System.EventHandler(this.waitingBack_Click);
+            // 
+            // waitingGridView
+            // 
+            this.waitingGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.waitingGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4});
+            this.waitingGridView.Location = new System.Drawing.Point(265, 121);
+            this.waitingGridView.Name = "waitingGridView";
+            this.waitingGridView.Size = new System.Drawing.Size(240, 150);
+            this.waitingGridView.TabIndex = 0;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Position in Queue";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "License Plate";
+            this.Column4.Name = "Column4";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(800, 450);
+            this.panel4.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "History";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Waiting List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Park or Unpark";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.historyGridView);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 450);
+            this.panel3.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(348, 287);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // historyGridView
+            // 
+            this.historyGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.historyGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Action,
+            this.licensePlate,
+            this.Time});
+            this.historyGridView.Location = new System.Drawing.Point(256, 92);
+            this.historyGridView.Name = "historyGridView";
+            this.historyGridView.Size = new System.Drawing.Size(343, 150);
+            this.historyGridView.TabIndex = 1;
+            // 
+            // Action
+            // 
+            this.Action.HeaderText = "Action";
+            this.Action.Name = "Action";
+            // 
+            // licensePlate
+            // 
+            this.licensePlate.HeaderText = "License Plate";
+            this.licensePlate.Name = "licensePlate";
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.parkingGridView)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.waitingGridView)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.historyGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView parkingGridView;
+        private System.Windows.Forms.DataGridView waitingGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button waitingBack;
+        private System.Windows.Forms.Button parkBack;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView historyGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Action;
+        private System.Windows.Forms.DataGridViewTextBoxColumn licensePlate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+    }
+}
+
---
+++ Parking/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parking
+{
+    public partial class Form1 : Form
+    {
+        // parking slot
+        private string[] parkingSlots;
+        // waiting list
+        private Queue<string> waitingQueue = new Queue<string>();
+        // history
+        private Stack<string> parkingHistory = new Stack<string>();
+        // for unique plate numbers
+        private HashSet<string> parkedCars = new HashSet<string>();
+        // specifying parking lot size
+        private int parkingLotSize = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            parkingSlots = new string[parkingLotSize];
+            UpdateUI();
+        }
+        private void ParkCar(string plateNumber)
+        {
+            string timeFormat = "HH/mm";
+            if (string.IsNullOrWhiteSpace(plateNumber))
+            {
+                MessageBox.Show("Please enter a valid license plate.");
+                return;
+            }
+            if (parkedCars.Contains(plateNumber))
+            {
+                MessageBox.Show("This car is already parked.");
+                return;
+            }
+            // searches for an empty slot
+            for (int i = 0; i < parkingSlots.Length; i++)
+            {
+                if (parkingSlots[i] == null)
+                {
+                    parkingSlots[i] = plateNumber;
+                    parkedCars.Add(plateNumber);
+                    parkingHistory.Push($"Parked|{plateNumber}|{DateTime.Now.ToString(timeFormat)}");
+                    UpdateUI();
+                    return;
+                    
+                }
+            }
+            // add to waiting list
+            waitingQueue.Enqueue(plateNumber);
+            MessageBox.Show("Parking lot is full. Added to the waiting list.");
+            UpdateUI();
+        }
+        private void UnparkCar(string plateNumber)
+        {
+            string timeFormat = "HH/mm";
+            if (!parkedCars.Contains(plateNumber))
+            {
+                MessageBox.Show("Car not found in the parking lot.");
+                return;
+            }
+            // unpark
+            for (int i = 0; i < parkingSlots.Length; i++)
+            {
+                if (parkingSlots[i] == plateNumber)
+                {
+                    parkingSlots[i] = null;
+                    parkedCars.Remove(plateNumber);
+                    parkingHistory.Push($"Unparked|{plateNumber}|{DateTime.Now.ToString(timeFormat)}");
+                    MessageBox.Show($"Car {plateNumber} has been removed.");
+                    break;
+                }
+            }
+            // moves car from waiting list to the parking lot
+            if (waitingQueue.Count > 0)
+            {
+                string nextCar = waitingQueue.Dequeue();
+                for (int i = 0; i < parkingSlots.Length; i++)
+                {
+                    if (parkingSlots[i] == null)
+                    {
+                        parkingSlots[i] = nextCar;
+                        parkedCars.Add(nextCar);
+                        parkingHistory.Push($"Unparked|{plateNumber}|{DateTime.Now.ToString(timeFormat)}");
+                        MessageBox.Show($"Car {nextCar} has been parked from the waiting list.");
+
+                        break;
+                    }
+                }
+            }
+            UpdateUI();
+        }
+        private void UpdateWaitingList()
+        {
+            waitingGridView.Rows.Clear();
+
+            int position = 1;
+            foreach (string plateNumber in waitingQueue)
+            {
+                waitingGridView.Rows.Add(position++, plateNumber);
+            }
+        }
+        private void UpdateHistory()
+        {
+            historyGridView.Rows.Clear();
+            foreach (string action in parkingHistory)
+            {
+                string[] details = action.Split('|');
+                historyGridView.Rows.Add(details[0], details[1], details[2]);
+            }
+        }
+        private void UpdateParkingStatus()
+        {
+            parkingGridView.Rows.Clear();
+            for (int i = 0; i < parkingSlots.Length; i++)
+            {
+                string slotStatus = parkingSlots[i] == null ? "Available" : "Occupied";
+                string plateNumber = parkingSlots[i] ?? "Empty";
+                // adds row to the data grid view
+                parkingGridView.Rows.Add($"Slot {i + 1}", plateNumber, slotStatus);
+            }
+        }
+        private void parkingGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                string slotNumber = parkingGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
+                string plateNumber = parkingGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+                MessageBox.Show($"Selected {slotNumber} with license plate: {plateNumber}");
+            }
+        }
+        private void UpdateUI()
+        {
+            UpdateParkingStatus();
+            UpdateWaitingList();
+            UpdateHistory();
+        }
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            string plateNumber = txtLicense.Text;
+            ParkCar(plateNumber);
+            txtLicense.Clear();
+        }
+        private void btnRemove_Click_1(object sender, EventArgs e)
+        {
+            string plateNumber = txtLicense.Text;
+            UnparkCar(plateNumber);
+            txtLicense.Clear();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel4.Visible = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+            panel3.Visible = false;
+            panel4.Visible = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = true;
+            panel4.Visible = false;
+        }
+        private void waitingBack_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = true;
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = false;
+        }
+        private void parkBack_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = true;
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = false;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = true;
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = false;
+        }
+    }
+}
